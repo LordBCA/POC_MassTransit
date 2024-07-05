49759cb50cc4bf779e48fb40d0f84fedf0262b80
Diego Bocanegra <diegoa_boca@hotmail.com>
2024-07-05 10:32:42 -0500

    revert extensions

--- src/POC_MassTransit.Infrastructure/Messaging/MessageBrokerExtensions.cs
+++ src/POC_MassTransit.Infrastructure/Messaging/MessageBrokerExtensions.cs
@@ -11,23 +11,40 @@ public static class MessageBrokerExtensions
 {
     public static IServiceCollection AddMessageBroker
         (this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null)
-    {        
+    {
         var messageBrokerOptions = configuration.GetSection("MessageBroker").Get<MessageBrokerOptions>();
-
-        services.AddSingleton<MessageBrokerConfiguratorFactory>();
 
         services.AddMassTransit(config =>
         {
             config.SetKebabCaseEndpointNameFormatter();
 
-            config.AddConsumers(assembly);            
+            config.AddConsumers(assembly);
 
-            var provider = services.BuildServiceProvider();
-            var factory = provider.GetRequiredService<MessageBrokerConfiguratorFactory>();
-            var configurator = factory.Create();
-            configurator.Configure(config);
-               
-        });        
+            switch (messageBrokerOptions.Service)
+            {
+                case "AzureServiceBus":
+                    config.UsingAzureServiceBus((context, configurator) =>
+                    {
+                        configurator.Host(messageBrokerOptions.ConnectionString);
+                        configurator.ConfigureEndpoints(context);
+                    });
+                    break;
+                case "RabbitMQ":
+                    config.UsingRabbitMq((context, configurator) =>
+                    {
+                        configurator.Host(new Uri(messageBrokerOptions.Host!), host =>
+                        {
+                            host.Username(messageBrokerOptions.UserName);
+                            host.Password(messageBrokerOptions.Password);
+                        });
+                        configurator.ConfigureEndpoints(context);
+                    });
+                    break;
+                default:
+                    config.UsingInMemory();
+                    break;
+            }
+        });
 
         services.AddSingleton<IMessageBrokerService, MessageBrokerService>();
 
